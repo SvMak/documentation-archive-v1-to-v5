219cfd91acf1770eed27b6a5ff87ec0ea7b6b602
Sviatoslav Makhynko <sviat.mak@gmail.com>
2024-09-19 23:01:51 +0200

    fix: v4.8.2

--- v4.8.2/js/services.js
+++ v4.8.2/js/services.js
@@ -60,7 +60,7 @@
         var currentModule;
         return {
             getModulesConfig: function () {
-                return $http.get('/modules/marketplace/data/modules-config.json').success(function (resp) {
+                return $http.get('/documentation-archive-v1-to-v5/v4.8.2/modules/marketplace/data/modules-config.json').success(function (resp) {
                     return resp;
                 });
             },
