f1b562cc0157958d4a7a3252d8369d6fc894f20a
Sviatoslav Makhynko <sviat.mak@gmail.com>
2024-09-19 22:40:05 +0200

    fix: v4.13.2

--- v4.13.2/js/marketplace-list.js
+++ v4.13.2/js/marketplace-list.js
@@ -4,7 +4,7 @@
     angular.module('marketplace.list', ['ngRoute', 'jhipster.service'])
         .config(['$routeProvider', function ($routeProvider) {
             $routeProvider.when('/list', {
-                templateUrl: '/modules/marketplace/list/list.html',
+                templateUrl: '/documentation-archive-v1-to-v5/v4.13.2/modules/marketplace/list/list.html',
                 controller: 'ModuleListCtrl'
             });
         }])
